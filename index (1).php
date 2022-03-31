a0b82a2983dbc2c16c4473629fc9f52c4ca0ec8b
AkubsDickson <68328217+AkubsDickson@users.noreply.github.com>
2022-03-31 15:49:06 +0000

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,1 @@
+<?php    $msisdn = $_GET['msisdn'];    $sequence_ID = $_GET['sequenceID'];    $data = $_GET['data'];    $type = 1;      $mereply = "Welcome To Ashesi Mobile App 2022\n     1. choose a movie\n     2. select a music\n     3. thank you\n";    echo $mereply.'|'.$msisdn.'|'.$sequence_id.'|'.$type;?>
